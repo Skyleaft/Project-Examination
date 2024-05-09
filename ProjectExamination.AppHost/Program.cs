5f4bb128f67e90ae87b656383642296c1019aa09
Milzan Malik <milzan_malik@outlook.com>
2024-05-09 18:36:59 +0700

    rename web, added aspire

---
+++ ProjectExamination.AppHost/Program.cs
@@ -0,0 +1,7 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var api =builder.AddProject<Projects.API>("api");
+var web = builder.AddProject<Projects.Web>("web");
+
+api.WithReference(web);
+builder.Build().Run();
