3aae8d026bf3dd296a9322a996ccf474960739e0
Milzan Malik <milzan_malik@outlook.com>
2024-06-24 16:20:39 +0700

    update bobot point soal

--- Shared/BankSoal/Soal.cs
+++ Shared/BankSoal/Soal.cs
@@ -7,5 +7,14 @@ public class Soal
     public int Nomor { get; set; }
     public string Pertanyaan { get; set; }
     public List<SoalJawaban> PilihanJawaban { get; set; }
-    public int BobotPoint { get; set; }
+    public bool isMultipleJawaban { get; set; }
+    public int BobotPoint
+    {
+        get
+        {
+            if (PilihanJawaban != null)
+                return PilihanJawaban.Sum(x => x.Point);
+            return 0;
+        }
+    }
 }
---
+++ Web/Migrations/20240624090306_v1.1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class v11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BobotPoint",
+                table: "Soal");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isMultipleJawaban",
+                table: "Soal",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isMultipleJawaban",
+                table: "Soal");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BobotPoint",
+                table: "Soal",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
